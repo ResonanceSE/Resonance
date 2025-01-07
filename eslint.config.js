65592ff6877af5cfea835d0a6ba597cd1e00993c
KKhamwiset <kritsakorn224@gmail.com>
2025-01-07 19:14:25 +0700

    Modified eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import pluginVue from "eslint-plugin-vue";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/essential"],
