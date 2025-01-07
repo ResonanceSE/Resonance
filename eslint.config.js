eb8e51f7449cd41b16e2b537934d565b76f60420
KKhamwiset <kritsakorn224@gmail.com>
2025-01-07 20:08:29 +0700

    Fix Eslint didn't work with nuxt

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,10 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
+import withNuxt from './.nuxt/eslint.config.mjs'
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
-];
+export default withNuxt(
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      'no-console': 'off'
+    }
+  },
+)
