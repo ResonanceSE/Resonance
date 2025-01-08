0ead2b5f345ace5203e81d933d895412438f0a2f
KKhamwiset <kritsakorn224@gmail.com>
2025-01-08 18:48:25 +0700

    Fix Linter too many blank line (110)

--- backend/settings.py
+++ backend/settings.py
@@ -60,8 +60,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
-
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -104,23 +102,15 @@ AUTH_PASSWORD_VALIDATORS = [
         ),
     },
 ]
-
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = 'static/'
-
 # Default primary key fie   ld type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
