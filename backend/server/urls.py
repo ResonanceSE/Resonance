e9df68c7ea92d3724214e638e9572750c510ae50
KKhamwiset <kritsakorn224@gmail.com>
2025-02-22 02:17:02 +0700

    Fix Linter

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -18,8 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from server.controller.product_controller import (get_all_products,
-                                                  get_product_by_name)
+from server.controller.product_controller import get_all_products, get_product_by_name
 
 from .views import home_view
 
