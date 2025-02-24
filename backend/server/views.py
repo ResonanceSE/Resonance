cd1303ea2558e61d19613d23aff92e50fa2bb07e
KKhamwiset <kritsakorn224@gmail.com>
2025-02-24 20:57:31 +0700

    Fix Linter

--- backend/server/views.py
+++ backend/server/views.py
@@ -1,7 +1,5 @@
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
 
 
 def home_view(request):
