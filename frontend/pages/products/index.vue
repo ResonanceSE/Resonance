731c4d2042e13577f6228ef000530008d55624be
KKhamwiset <kritsakorn224@gmail.com>
2025-03-09 20:41:36 +0700

    Added  Contact UI and live chat UI

--- frontend/pages/products/index.vue
+++ frontend/pages/products/index.vue
@@ -4,7 +4,6 @@
 
 <template>
     <div>
-        <navbar_header />
         <div class="container mx-auto py-10">
             <h1 class="text-2xl font-bold text-center mb-8">Product</h1>
         </div>
---
+++ frontend/pages/register.vue
@@ -0,0 +1,10 @@
+<script>
+
+
+</script>
+
+<template>
+    <div class="min-h-screen bg-gray-50 p-4 md:p-8 grid place-items-center">
+        <div class="relative w-full max-w-6xl rounded-3xl overflow-hidden shadow-xl bg-blue-50"/>
+    </div>
+</template>
