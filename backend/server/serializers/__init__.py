cb9bac50af636a5b61760e0ab6934d006b764ddd
KKhamwiset <kritsakorn224@gmail.com>
2025-03-28 19:45:17 +0700

    Fix Linter

--- backend/server/serializers/__init__.py
+++ backend/server/serializers/__init__.py
@@ -1,4 +1,5 @@
 from .customer_serializer import LoginSerializer
 from .product_serializer import ProductSerializer
 from .category_serializer import CategorySerializer
+
 __all__ = ["LoginSerializer", "ProductSerializer", "CategorySerializer"]
--- backend/server/serializers/category_serializer.py
+++ backend/server/serializers/category_serializer.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from ..models import Category
 
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name', 'slug']
+        fields = ["id", "name", "slug"]
