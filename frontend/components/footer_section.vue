092b1c75392ec62f929dac1ddc4a8fb63a0730f5
KKhamwiset <kritsakorn224@gmail.com>
2025-03-29 23:46:52 +0700

    Handle addtoCart activity, make user log in first

--- frontend/components/footer_section.vue
+++ frontend/components/footer_section.vue
@@ -9,14 +9,6 @@ const currentYear = new Date().getFullYear();
       <h2 class="text-xl font-bold text-white mb-2">Resonance Sound Shop</h2>
       <p class="text-white text-sm">Your destination for premium audio equipment and accessories</p>
     </div>
-    <nav>
-      <div class="grid grid-flow-col gap-6">
-        <a class="link link-hover"><Icon name="logos:x" class="h-8 w-8" aria-hidden="true"/></a>
-        <a class="link link-hover"><Icon name="logos:facebook" class="h-8 w-8" aria-hidden="true"/></a>
-        <a class="link link-hover"><Icon name="logos:youtube-icon" class="h-8 w-8" aria-hidden="true"/></a>
-        <a class="link link-hover"><Icon name="logos:instagram-icon" class="h-8 w-8" aria-hidden="true"/></a>
-      </div>
-    </nav>
     <aside>
       <p class="text-white">Copyright © {{ currentYear }} - All rights reserved by Resonance Sound Shop</p>
     </aside>
